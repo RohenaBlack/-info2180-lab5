c77df1262e050612781d37acd54cc0e005892c43
RohenaBlack <rohena889@gmail.com>
2022-11-12 03:59:07 -0500

    Completed exercise 5

--- world.js
+++ world.js
@@ -1,5 +1,6 @@
 window.onload = event =>{
     var searchBtn = document.getElementById('lookup');
+    var citiesLookup = document.getElementById('citiesSearchBtn');
     var country = document.getElementById('country');
     var countryInfo = document.getElementById('result');
 
@@ -14,6 +15,20 @@ window.onload = event =>{
         }
         /* Sending a GET request to the world.php file with the country name as a parameter. */
         httpReq.open('GET', `world.php?country=${country.value.trim()}`);
+        /* Sending the request to the server. */
+        httpReq.send();
+    }
+
+
+    citiesLookup.onclick = event =>{
+        /* Creating a new XMLHttpRequest object. */
+        var httpReq = new XMLHttpRequest();
+        httpReq.onreadystatechange = function(){
+            if(httpReq.readyState==4 && httpReq.status==200){
+               countryInfo.innerHTML = httpReq.response;
+            }
+        }
+        httpReq.open('GET', `world.php?country=${country.value.trim()}&context=cities`);
         /* Sending the request to the server. */
         httpReq.send();
     }
--- world.php
+++ world.php
@@ -8,16 +8,25 @@ $conn = new PDO("mysql:host=$host;dbname=$dbname;charset=utf8mb4", $username, $p
 
 /* Sanitizing the input from the user. */
 $country = filter_input(INPUT_GET, 'country', FILTER_SANITIZE_STRING);
+$context = filter_input(INPUT_GET, "context", FILTER_SANITIZE_STRING);
 
 /* Selecting all the countries from the database where the name is like the country variable. */
 $stmt = $conn->query("SELECT * FROM countries WHERE name LIKE '%$country%' ");
-
-
 $results = $stmt->fetchAll(PDO::FETCH_ASSOC);
+
+/* Selecting the name, district, and population of the cities from the cities table and joining it with
+the countries table on the country code. It is then selecting the countries where the name is like
+the country variable. */
+$city_info= $conn->query("SELECT cities.name, cities.district, cities.population
+FROM cities LEFT JOIN countries ON countries.code = cities.country_code
+WHERE countries.name LIKE '%$country%'");
+
+/* Fetching all the results from the query and putting them into an associative array. */
+$city= $city_info->fetchAll(PDO::FETCH_ASSOC);
 ?>
 
 
-<body>
+<?php if(isset($country)&&(!isset($context))):?>
   <table id="resultsTable">
     <thead>
       <tr>
@@ -43,5 +52,36 @@ $results = $stmt->fetchAll(PDO::FETCH_ASSOC);
       ?>
     </tbody>
   </table>
-  
-</body>
+  <?php endif; ?>
+
+
+<?php if (isset($context)):?>
+  <table id="resultsTable">
+    <thead>
+      <tr>
+        <th>City</th>
+        <th>District</th>
+        <th>Population</th>
+      </tr>
+    </thead>
+
+    <tbody>
+      <?php
+      foreach($city as $cityRecord){
+        ?>
+          <tr>
+            <td> <?= $cityRecord['name']; ?></td>
+            <td> <?= $cityRecord['district']; ?></td>
+            <td> <?= $cityRecord['population']; ?></td>
+          </tr>
+        <?php
+      }
+      ?>
+    </tbody>
+  </table>
+<?php endif ?>
+
+
+
+
+
